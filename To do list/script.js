f5ec9c58c4a37468d3d4c45ef62696193a7ccf6c
Akretche Ales Samy <131418700+samAK02@users.noreply.github.com>
2023-11-23 12:55:27 +0100

    Add files via upload

---
+++ To do list/script.js
@@ -0,0 +1,44 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("List-container");
+const addButton = document.getElementById("add-button");
+
+
+
+
+
+addButton.addEventListener("click", addTask);
+
+function addTask() { 
+    const taskText = inputBox.value.trim();
+    if (taskText === '') {
+        alert("Please write something"); 
+    } else {
+        const li = document.createElement("li");
+        li.textContent = taskText;
+        
+        
+        li.addEventListener("click", toggleTask);
+
+        listContainer.appendChild(li);
+        inputBox.value = '';
+
+        const span = document.createElement("span");
+        span.className = "span";
+        span.innerHTML = "\u00d7";
+        span.addEventListener("click", deleteTask);
+        li.appendChild(span);
+    }
+}
+
+function toggleTask(event) {
+    const li = event.target;
+    li.classList.toggle("checked");
+}
+
+function deleteTask(event) {
+    const span = event.target;
+    const li = span.parentElement;
+    li.remove();
+}
+
+ 
